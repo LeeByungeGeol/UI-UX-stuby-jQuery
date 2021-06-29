86ef23a4c9ad2fa495b91b6614e018b1ad3fb6f0
LeeByungeGeol <80818570+LeeByungeGeol@users.noreply.github.com>
2021-06-30 02:10:43 +0900

    Add files via upload

---
+++ 87_parallax.js
@@ -0,0 +1,12 @@
+$(function () {
+    // 원도우의 스크롤 이벤트가 발생하면 스크롤의 위치값에 따라서
+    // .parallax안에 배경으로 넣을 그림을 위로, 아래로 움직이도록 background-position y축값을 조절
+    $(window).scroll(function () {
+        var yScroll = $(this).scrollTop();
+        console.log(yScroll);
+        var moveY = yScroll * 0.1;
+        // $("#container>.parallax").css("background-position", "center " + moveY + "px");
+        $("#container>.parallax").css("background-position", `center ${moveY}px`);
+
+    })
+})
